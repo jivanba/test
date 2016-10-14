9762f4388fd0f23538cfb3f9a3a89182f36f292e
jivanba <jivanba.icloud.com>
2016-10-14 00:23:58 -0500

    first commit

---
+++ miniReto.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var serie = 0...100
+
+for n in serie {
+    
+    if n > 30 && n < 40{
+        
+        
+        print("\(n)Viva Swift!!!")
+        
+    }
+    
+    else if (n % 5) == 0 {
+        
+        print("\(n)Bingo!!!")
+        
+    }
+    
+    else if (n % 2) == 0 {
+        
+        print("\(n)Par!!!")
+        
+    }
+    
+    else if (n % 2) != 0 {
+        
+        print("\(n)Impar!!!")
+        
+    }
+    
+        }
+
+    
+    
+
+
+
